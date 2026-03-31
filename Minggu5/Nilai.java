35f69242f08ea343ae041a8224e162390b65c4f9
Alden <aldendzakwan@gmail.com>
2026-03-31 16:44:21 +0700

    Tugas_minggu5

---
+++ Minggu5/Nilai.java
@@ -0,0 +1,18 @@
+package Minggu5;
+
+public class Nilai {
+
+    String namaMahasiswa;
+    String nomorInduk;
+    int angkatan;
+    int uts;
+    int uas;
+
+    public Nilai(String namaMahasiswa, String nomorInduk, int angkatan, int uts, int uas) {
+        this.namaMahasiswa = namaMahasiswa;
+        this.nomorInduk = nomorInduk;
+        this.angkatan = angkatan;
+        this.uts = uts;
+        this.uas = uas;
+    }
+}   
