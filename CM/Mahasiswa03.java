982e14f43a18ed3f02734f73739c2d500fe3fe95
Alden <aldendzakwan@gmail.com>
2026-04-23 09:05:49 +0700

    Percobaan1_jobsheet9

--- CM/Mahasiswa03.java
+++ CM/Mahasiswa03.java
@@ -3,14 +3,18 @@ public class Mahasiswa03 {
     String nim;
     String nama;
     String prodi;
+    // Jawaban sesi 2 A
+    boolean statusMember;
 
-    Mahasiswa03(String nim, String nama, String prodi) {
+    Mahasiswa03(String nim, String nama, String prodi, boolean statusMember) {
         this.nim = nim;
         this.nama = nama;
         this.prodi = prodi;
+        this.statusMember = statusMember;
+
     }
 
     void tampilMahasiswa() {
-        System.out.println("NIM: " + nim + " | " + "Nama: " + nama + " | " + "Prodi: " + prodi);
+        System.out.println("NIM: " + nim + " | " + "Nama: " + nama + " | " + "Prodi: " + prodi + " | "  + "Status Member: " + (statusMember? "Member" : "Bukan"));
     }
 }
