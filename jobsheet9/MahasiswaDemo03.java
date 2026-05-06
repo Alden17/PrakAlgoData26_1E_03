ad19a96f900c5489a4a81bed2f445850e7789ca9
Alden <aldendzakwan@gmail.com>
2026-05-06 07:49:09 +0700

    Percobaan2_jobsheet9

--- jobsheet9/MahasiswaDemo03.java
+++ jobsheet9/MahasiswaDemo03.java
@@ -1,4 +1,5 @@
 package jobsheet9;
+
 import java.util.Scanner;
 
 public class MahasiswaDemo03 {
@@ -9,62 +10,63 @@ public class MahasiswaDemo03 {
         int pilih;
 
         do {
-    System.out.println("\nMenu:");
-    System.out.println("1. Mengumpulkan Tugas");
-    System.out.println("2. Menilai Tugas");
-    System.out.println("3. Melihat Tugas Pertama");
-    System.out.println("4. Melihat Daftar Tugas");
-    System.out.println("5. Melihat Jumlah Tugas Yang Dikumpulkan");
-    System.out.print("Pilih: ");
-    pilih = scan.nextInt();
-    scan.nextLine();
+            System.out.println("\nMenu:");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Melihat Tugas Pertama");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Jumlah Tugas Yang Dikumpulkan");
+            System.out.print("Pilih: ");
+            pilih = scan.nextInt();
+            scan.nextLine();
 
-    switch (pilih) {
-        case 1:
-            System.out.print("Nama: ");
-            String nama = scan.nextLine();
-            System.out.print("NIM: ");
-            String nim = scan.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = scan.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama: ");
+                    String nama = scan.nextLine();
+                    System.out.print("NIM: ");
+                    String nim = scan.nextLine();
+                    System.out.print("Kelas: ");
+                    String kelas = scan.nextLine();
 
-            Mahasiswa03 mhs = new Mahasiswa03(nama, nim, kelas);
-            stack.push(mhs);
-            System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
-            break;
+                    Mahasiswa03 mhs = new Mahasiswa03(nama, nim, kelas);
+                    stack.push(mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                    break;
 
-        case 2:
-            Mahasiswa03 dinilai = stack.pop();
-            if (dinilai != null) {
-                System.out.println("Menilai tugas dari " + dinilai.nama);
-                System.out.print("Masukkan nilai (0-100): ");
-                int nilai = scan.nextInt();
-                dinilai.tugasDinilai(nilai);
-                System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                case 2:
+                    Mahasiswa03 dinilai = stack.pop();
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari " + dinilai.nama);
+                        System.out.print("Masukkan nilai (0-100): ");
+                        int nilai = scan.nextInt();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas: " + biner);
+                    }
+                    break;
+
+                case 3:
+                    Mahasiswa03 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + lihat.nama);
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Daftar semua tugas");
+                    System.out.println("Nama\tNIM\tKelas");
+                    stack.print();
+                    break;
+
+                case 5:
+                    System.out.println("Jumlah tugas saat ini: " + stack.jumlahTugas());
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid.");
             }
-            break;
-
-        case 3:
-            Mahasiswa03 lihat = stack.peek();
-            if (lihat != null) {
-                System.out.println("Tugas pertama dikumpulkan oleh " + lihat.nama);
-            }
-            break;
-
-        case 4:
-            System.out.println("Daftar semua tugas");
-            System.out.println("Nama\tNIM\tKelas");
-            stack.print();
-            break;
-        
-        case 5:
-            System.out.println("Jumlah tugas saat ini: " + stack.jumlahTugas());
-            break; 
-            
-        default:
-            System.out.println("Pilihan tidak valid.");
-    }
-} while (pilih >= 1 && pilih <= 4);
+        } while (pilih >= 1 && pilih <= 4);
     }
 }
-
--- jobsheet9/StackTugasMahasiswa03.java
+++ jobsheet9/StackTugasMahasiswa03.java
@@ -66,4 +66,20 @@ public class StackTugasMahasiswa03 {
     public int jumlahTugas() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi03 stack = new StackKonversi03 ();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+
+        return biner;
+    }
 }
